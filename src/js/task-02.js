551c8e187cb2f971c0167974c429fc23ff3908db
karolinabrz <120278636+karolinabrz@users.noreply.github.com>
2022-12-14 22:35:21 +0100

    added task 10

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,19 +1,21 @@
 const ingredients = [
-	"Potatoes",
-	"Mushrooms",
-	"Garlic",
-	"Tomatos",
-	"Herbs",
-	"Condiments",
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 const $ul = document.querySelector("ul");
 const fragment = document.createDocumentFragment();
 
 ingredients.forEach((ing) => {
-	const newItem = document.createElement("li");
-	newItem.textContent = ing;
-	newItem.classList = "item";
-	fragment.append(newItem);
+  const newItem = document.createElement("li");
+  newItem.textContent = ing;
+  newItem.classList = "item";
+  fragment.append(newItem);
 });
 
 $ul.append(fragment);
+
+
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,45 @@
+("use strict");
+const { log } = console;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const $inputElement = document.querySelector("input");
+const $createButton = document.querySelector("[data-create]");
+const $destroyButton = document.querySelector("[data-destroy]");
+const $divBox = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i++) {
+    let element = document.createElement("div");
+
+    if (i > 0) {
+      width += 10;
+      height += 10;
+    }
+
+    element.style.width = `${width}px`;
+    element.style.height = `${height}px`;
+
+    element.style.backgroundColor = getRandomHexColor();
+    $divBox.insertAdjacentElement("afterbegin", element);
+    $inputElement.disabled = true;
+    $createButton.disabled = true;
+  }
+};
+
+$createButton.addEventListener("click", (e) => {
+  createBoxes($inputElement.value);
+});
+
+$destroyButton.addEventListener("click", (e) => {
+  $inputElement.disabled = false;
+  $createButton.disabled = false;
+  while ($divBox.firstChild) {
+    $divBox.firstChild.remove();
+  }
+});
